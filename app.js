9b1bbca6e1f6a0aa662230a25d3bf3da0a654e44
shrutiantil <antilshruti879@gmail.com>
2026-07-25 21:24:51 +0530

    changes

---
+++ app.js
@@ -0,0 +1,35 @@
+let Boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+
+
+let turnO=true;
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6], 
+    [0,4,8],
+    [1,4,7], 
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]  ];
+
+
+Boxes.forEach((box) => {
+    box.addEventListener("click",() =>
+         {console.log("box was clicked");
+            if(turnO ===true)
+                {box.innerText="O";
+                turnO=false;}
+
+            else{box.innerText="X";
+                 turnO=true;}
+            
+   box.disabled=true;
+            
+         })
+    });
+
+
+ const checkWinner=() => {for( let pattern of winPatterns){console.log(pattern);}}    
+    
